6a05ef9ad0281ffed82078df47f311d5ce5926d1
Jonatan Missora <jonatanjmissora@gmail.com>
2025-12-10 13:30:45 -0300

    Revert "creando animaciones en home"
    This reverts commit 609b7a3f4cb5176104ab251c8ce5365d8e2aefbd.

--- src/_components/01_home/page.tsx
+++ src/_components/01_home/page.tsx
@@ -52,16 +52,9 @@ const LeafImgs = () => {
 const Hero = () => {
 	return (
 		<article className="absolute z-1 flex flex-col justify-center items-center hero-container">
-			<div className="flex flex-col gap-6 relative">
-				<div className="relative h-14 w-100 overflow-hidden">
-					<p className="hero-title">Centro de estética</p>
-				</div>
-				<div className="absolute top-1/2 -translate-1/2 left-1/2 h-6">
-					<p className="hero-title-y delay660 text-4xl leading-[25px]">&</p>
-				</div>
-				<div className="relative h-14 w-100 overflow-hidden">
-					<p className="hero-title delay330">bienestar integral</p>
-				</div>
+			<div className="hero-title">
+				<p>Centro cosmiátrico</p>
+				<p>de belleza</p>
 			</div>
 			<div>
 				<p className="hero-subtitle">Al cuidado de tu piel.</p>
@@ -71,7 +64,7 @@ const Hero = () => {
 				href="https://alba-estetica-turnos.netlify.app/"
 				target="_blank"
 				rel="noopener noreferrer"
-				className="cta-button text-center mt-4 delay660"
+				className="cta-button text-center mt-4"
 			>
 				RESERVA TU TURNO
 			</Link>
@@ -80,22 +73,22 @@ const Hero = () => {
 }
 
 const Skills = () => {
-	const skillData = [
-		{ icon: "+5", content: "años de experiencia" },
-		{ icon: "+120", content: "clientas atendidas" },
-		{ icon: "99.9%", content: "clientas satisfechas" },
-	]
 	return (
 		<article className="absolute sm:bottom-12 2xl:bottom-24 left-(--sm-layout-padding) 2xl:left-(--2xl-layout-padding) z-1 flex sm:gap-6 2xl:gap-10 items-center justify-center">
-			{skillData.map(skill => (
-				<div
-					key={skill.icon}
-					className="flex items-center justify-center gap-2 rounded-tl-xl rounded-br-xl bg-(--primary-pink) border border-[#444]/10 custom-shadow hero-skill delay1330"
-				>
-					<i>{skill.icon}</i>
-					<span>{skill.content}</span>
-				</div>
-			))}
+			<div className="flex items-center justify-center gap-2 rounded-tl-xl rounded-br-xl bg-(--primary-pink) border border-[#444]/10 custom-shadow hero-skill">
+				<i>+5</i>
+				<span>años de experiencia</span>
+			</div>
+
+			<div className="flex items-center justify-center gap-2 rounded-tl-xl rounded-br-xl bg-(--primary-pink) border border-[#444]/10 custom-shadow hero-skill">
+				<i>+120</i>
+				<span>clientas atendidas</span>
+			</div>
+
+			<div className="flex items-center justify-center gap-2 rounded-tl-xl rounded-br-xl bg-(--primary-pink) border border-[#444]/10 custom-shadow hero-skill">
+				<i>99.9%</i>
+				<span>clientas satisfechas</span>
+			</div>
 		</article>
 	)
 }
